7b10b859440ce816756e8ec44dc751dd25ae01e7
oceanthunder <admin@oceanthunder.dev>
2026-06-14 00:42:13 +0530

    picom config

--- link.sh
+++ link.sh
@@ -1,14 +1,16 @@
-DOTFILES_DIR="$HOME/.dotfiles"
+DOTFILES="$HOME/.dotfiles"
 
 echo "creating directories..."
 mkdir -p "$HOME/.config/i3"
 mkdir -p "$HOME/.config/i3status"
 mkdir -p "$HOME/.config/alacritty"
+mkdir -p "$HOME/.config/picom"
 
 echo "creating symbolic links..."
-ln -sf "$DOTFILES_DIR/bashrc" "$HOME/.bashrc"
-ln -sf "$DOTFILES_DIR/i3.conf" "$HOME/.config/i3/config"
-ln -sf "$DOTFILES_DIR/i3status.conf" "$HOME/.config/i3status/config"
-ln -sf "$DOTFILES_DIR/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+ln -sf "$DOTFILES/bashrc" "$HOME/.bashrc"
+ln -sf "$DOTFILES/i3.conf" "$HOME/.config/i3/config"
+ln -sf "$DOTFILES/i3status.conf" "$HOME/.config/i3status/config"
+ln -sf "$DOTFILES/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+ln -sf "$DOTFILES/picom.conf" "$HOME/.config/picom/picom.conf"
 
 echo "done!"
